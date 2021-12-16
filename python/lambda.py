3c5d14aedd519f9e88cf42303c8eed97ce715008
baptiste-pauletto1 <baptistep.contact@gmail.com>
2021-12-16 13:05:58 +0100

    update(lambda.py): progress concerning S3 updates

--- python/lambda.py
+++ python/lambda.py
@@ -1,11 +1,45 @@
 import json
+import boto3
+import pandas
+import csv
+
+def write_newline_to_csv(newline, filename, bucket):
+    
+    try:
+        # Catching all variables from the incoming message
+        bucket = event.get(bucket)
+        key = event.get(key)
+        id_customer = event.get(id_customer)
+        id_ticket = event.get(id_ticket)
+        time = event.get(time)
+        
+        # Get file from S3 using boto3
+        s3 = boto3.client('s3')
+        s3_object = s3_client.get_object(Bucket=BUCKET,
+                                        Key=KEY)
+        
+        # Getting the CSV from the bucket
+        csv_s3 = response["Body"]
+        
+        # Turning result into dataframe
+        df_s3 = pd.read_csv(csv_s3, index_col = 0)
+        
+        # Adding the new row 
+        df_s3.append([id_customer,id_ticket,time])
+        
+        # Putting back the csv file on S3
+        
+        
+    except:
+        
+        return {
+            'statusCode': 400,
+            'body': 'Error, could not complete the request!'}
 
 def lambda_handler(event, context):
     # TODO implement
-    print(f"Mon petit event : {event}")
-
     return {
         'statusCode': 200,
-        'body': event
+        'body': json.dumps('Hello from Lambda!')
     }
 
