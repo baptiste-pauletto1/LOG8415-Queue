64ec7af8bcbc2ae30c03e20ac2ad9cb09e3eb655
baptiste-pauletto1 <baptistep.contact@gmail.com>
2021-12-16 18:19:35 +0100

    update(structure): Updating guidelines and adding lambda function code to handle DynamoDB.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Repository of my personnal project in the **LOG8415 : Advanced Concepts of Cloud
 
 ## Queue analysis (*Analyse de files d'attentes*)
 
-The project's main goal is to get hands on with AWS services like **Lambda functions**, **Simple Queue Service** and **Simple Storage Service**  in order to complete a performance analysis of actual implementation of waiting queues using a serverless approach.
+The project's main goal is to get hands on with AWS services like **Lambda functions**, **Simple Queue Service** and **DynamoDB**  in order to complete a performance analysis of actual implementation of waiting queues using a serverless approach.
--- python/lambda.py
+++ python/lambda.py
@@ -1,45 +1,33 @@
 import json
 import boto3
-import pandas
-import csv
 
-def write_newline_to_csv(newline, filename, bucket):
-    
-    try:
-        # Catching all variables from the incoming message
-        bucket = event.get(bucket)
-        key = event.get(key)
-        id_customer = event.get(id_customer)
-        id_ticket = event.get(id_ticket)
-        time = event.get(time)
-        
-        # Get file from S3 using boto3
-        s3 = boto3.client('s3')
-        s3_object = s3_client.get_object(Bucket=BUCKET,
-                                        Key=KEY)
-        
-        # Getting the CSV from the bucket
-        csv_s3 = response["Body"]
-        
-        # Turning result into dataframe
-        df_s3 = pd.read_csv(csv_s3, index_col = 0)
-        
-        # Adding the new row 
-        df_s3.append([id_customer,id_ticket,time])
-        
-        # Putting back the csv file on S3
-        
-        
-    except:
-        
-        return {
-            'statusCode': 400,
-            'body': 'Error, could not complete the request!'}
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    try:
+        # Getting our event parameters
+        table_name = event.get('table')
+        id = event.get('id')
+        customer = event.get('customer')
+        ticket = event.get('ticket')
+        time = event.get('time')
 
+        # Getting the dynamoDB resource
+        client = boto3.resource('dynamodb')
+
+        # Getting the concerned table
+        table = client.Table(table_name)
+
+        # Adding the new entry
+        table.put_item(Item={'id': id, 'customer': customer, 'ticket': ticket, 'time': time})
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f'Entry has conveniently been added to {table_name}')
+        }
+
+    except:
+
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Error ! Request was rejected')
+        }
